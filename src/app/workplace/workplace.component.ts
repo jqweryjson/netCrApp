81d2f3b6379a8e9d484a3fc6f214ec267317495c
kirillAkutin <jqweryjson@gmail.com>
2019-05-08 18:35:31 +0300

    layouts done

--- src/app/workplace/workplace.component.ts
+++ src/app/workplace/workplace.component.ts
@@ -6,10 +6,96 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./workplace.component.scss']
 })
 export class WorkplaceComponent implements OnInit {
-
+  todos:object[];
   constructor() { }
 
   ngOnInit() {
+    this.todos = [
+      {
+        id:0,
+        name:'RVPCE40',
+        vendor:'NEC',
+        description:'Lorem LoremLoremLoremLoremLoremLorem '
+      },
+      {
+        id:1,
+        name:'RVPCE40',
+        vendor:'NEC',
+        description:'Lorem LoremLoremLoremLoremLoremLorem '
+      },
+      {
+        id:2,
+        name:'RVPCE40',
+        vendor:'NEC',
+        description:'Lorem LoremLoremLoremLoremLoremLorem '
+      },
+      {
+        id:3,
+        name:'RVPCE40',
+        vendor:'NEC',
+        description:'Lorem LoremLoremLoremLoremLoremLorem '
+      },
+      {
+        id:4,
+        name:'RVPCE40',
+        vendor:'NEC',
+        description:'Lorem LoremLoremLoremLoremLoremLorem '
+      },
+      {
+        id:5,
+        name:'RVPCE40',
+        vendor:'NEC',
+        description:'Lorem LoremLoremLoremLoremLoremLorem '
+      },
+      {
+        id:6,
+        name:'RVPCE40',
+        vendor:'NEC',
+        description:'Lorem LoremLoremLoremLoremLoremLorem '
+      },
+      {
+        id:7,
+        name:'RVPCE40',
+        vendor:'NEC',
+        description:'Lorem LoremLoremLoremLoremLoremLorem '
+      },
+      {
+        id:8,
+        name:'RVPCE40',
+        vendor:'NEC',
+        description:'Lorem LoremLoremLoremLoremLoremLorem '
+      },
+      {
+        id:10,
+        name:'RVPCE40',
+        vendor:'NEC',
+        description:'Lorem LoremLoremLoremLoremLoremLorem '
+      },
+      {
+        id:11,
+        name:'RVPCE40',
+        vendor:'NEC',
+        description:'Lorem LoremLoremLoremLoremLoremLorem '
+      },
+      {
+        id:12,
+        name:'RVPCE40',
+        vendor:'NEC',
+        description:'Lorem LoremLoremLoremLoremLoremLorem '
+      },
+      {
+        id:13,
+        name:'RVPCE40',
+        vendor:'NEC',
+        description:'Lorem LoremLoremLoremLoremLoremLorem '
+      },
+      {
+        id:14,
+        name:'RVPCE40',
+        vendor:'NEC',
+        description:'Lorem LoremLoremLoremLoremLoremLorem '
+      },
+    ];
   }
 
 }
