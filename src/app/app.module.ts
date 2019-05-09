edd9660cbcaa2bfc333abbf023eed8b78fa0bcf0
kirillAkutin <jqweryjson@gmail.com>
2019-05-09 15:35:06 +0300

    filter is done! for me react is much better than angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
@@ -16,7 +18,9 @@ import { WorkplaceComponent } from './workplace/workplace.component';
     WorkplaceComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/workplace/workplace.component.ts
+++ src/app/workplace/workplace.component.ts
@@ -1,96 +1,125 @@
 import { Component, OnInit } from '@angular/core';
 
+interface ItoDo {
+  id:number,
+  version:string[],
+  name:string,
+  vendor:string,
+  description:string,
+}
 @Component({
   selector: 'app-workplace',
   templateUrl: './workplace.component.html',
   styleUrls: ['./workplace.component.scss']
 })
 export class WorkplaceComponent implements OnInit {
-  todos:object[];
-  constructor() { }
+  todos:ItoDo[];
+  searchText:string;
 
+  constructor() { }
+  get filterTodos():ItoDo[] {
+    if(this.searchText.trim().length === 0){
+      return this.todos;
+    }
+    return this.todos.filter( (item:ItoDo) => item.name.toLowerCase().includes(this.searchText.toLowerCase()) || item.description.toLowerCase().includes(this.searchText.toLowerCase()))
+  }
   ngOnInit() {
+    this.searchText = '';
     this.todos = [
       {
         id:0,
-        name:'RVPCE40',
+        version:['v1.001','v1.002','v1.003','v1.004','v1.005'],
+        name:'RVPrttg0',
         vendor:'NEC',
-        description:'Lorem LoremLoremLoremLoremLoremLorem '
+        description:'Lorem LoremLoremLoremLoremLoremLorem Lorem LoremLoremLoremLoremLoremLorem Lorem LoremLoremLoremLoremLoremLorem Lorem LoremLoremLoremLoremLoremLorem '
       },
       {
         id:1,
+        version:['v1.001','v1.002','v1.003','v1.004','v1.005'],
         name:'RVPCE40',
         vendor:'NEC',
         description:'Lorem LoremLoremLoremLoremLoremLorem '
       },
       {
         id:2,
+        version:['v1.001','v1.002','v1.003','v1.004','v1.005'],
         name:'RVPCE40',
         vendor:'NEC',
         description:'Lorem LoremLoremLoremLoremLoremLorem '
       },
       {
         id:3,
+        version:['v1.001','v1.002','v1.003','v1.004','v1.005'],
         name:'RVPCE40',
         vendor:'NEC',
         description:'Lorem LoremLoremLoremLoremLoremLorem '
       },
       {
         id:4,
+        version:['v1.001','v1.002','v1.003','v1.004','v1.005'],
         name:'RVPCE40',
         vendor:'NEC',
         description:'Lorem LoremLoremLoremLoremLoremLorem '
       },
       {
         id:5,
-        name:'RVPCE40',
+        version:['v1.001','v1.002','v1.003','v1.004','v1.005'],
+        name:'EWVPCE40',
         vendor:'NEC',
         description:'Lorem LoremLoremLoremLoremLoremLorem '
       },
       {
         id:6,
+        version:['v1.001','v1.002','v1.003','v1.004','v1.005'],
         name:'RVPCE40',
         vendor:'NEC',
         description:'Lorem LoremLoremLoremLoremLoremLorem '
       },
       {
         id:7,
-        name:'RVPCE40',
+        version:['v1.001','v1.002','v1.003','v1.004','v1.005'],
+        name:'UIYPCE40',
         vendor:'NEC',
         description:'Lorem LoremLoremLoremLoremLoremLorem '
       },
       {
         id:8,
-        name:'RVPCE40',
+        version:['v1.001','v1.002','v1.003','v1.004','v1.005'],
+        name:'UR40',
         vendor:'NEC',
         description:'Lorem LoremLoremLoremLoremLoremLorem '
       },
       {
         id:10,
+        version:['v1.001','v1.002','v1.003','v1.004','v1.005'],
         name:'RVPCE40',
         vendor:'NEC',
         description:'Lorem LoremLoremLoremLoremLoremLorem '
       },
       {
         id:11,
+        version:['v1.001','v1.002','v1.003','v1.004','v1.005'],
         name:'RVPCE40',
         vendor:'NEC',
         description:'Lorem LoremLoremLoremLoremLoremLorem '
       },
       {
         id:12,
+        version:['v1.001','v1.002','v1.003','v1.004','v1.005'],
         name:'RVPCE40',
         vendor:'NEC',
         description:'Lorem LoremLoremLoremLoremLoremLorem '
       },
       {
         id:13,
+        version:['v1.001','v1.002','v1.003','v1.004','v1.005'],
         name:'RVPCE40',
         vendor:'NEC',
         description:'Lorem LoremLoremLoremLoremLoremLorem '
       },
       {
         id:14,
+        version:['v1.001','v1.002','v1.003','v1.004','v1.005'],
         name:'RVPCE40',
         vendor:'NEC',
         description:'Lorem LoremLoremLoremLoremLoremLorem '
